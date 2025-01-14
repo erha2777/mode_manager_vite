31cafa5698a7b29d9578d3c049645cacdb1668ca
v_jaweiwang <v_jaweiwang@tencent.com>
2025-01-14 18:00:41 +0800

    feat: 新增设置弹窗

---
+++ src/components/SettingDialog/index.vue
@@ -0,0 +1,29 @@
+<!-- SettingDialog -->
+<script setup lang="ts">
+// import { reactive, ref } from 'vue'
+
+
+const {  } = defineProps({
+    
+})
+
+const emit = defineEmits(['closeEdit'])
+// 关闭编辑弹窗
+const closeEdit = () => {
+    emit('closeEdit')
+}
+
+</script>
+
+<template>
+    <div class="dialog dialog_edit" >
+        <div class="dialog__mask" @click="closeEdit"></div>
+        <div class="dialog__body">
+            12333
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use './index.scss';
+</style>
