8c9469a2fdac39678da3475f6bfe89f278264cc3
v_jaweiwang <v_jaweiwang@tencent.com>
2025-02-05 16:07:03 +0800

    style: 修改缩小样式

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './styles/common.css'
 import './styles/index.scss'
+import './styles/media.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
